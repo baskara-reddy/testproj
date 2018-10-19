fe2201cd0f9119ce1fe04c118689439ce641c1b5
BASKARA REDDY <baskara.ar@gmail.com>
2018-10-19 12:40:00 -0700

    updated print statement

--- javatest.java
+++ javatest.java
@@ -2,7 +2,7 @@ public class javatest {
 
     public static void main(String[] args) {
         // Prints "Hello, BOSS...Whatzup" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, BOSS...Whatzup");
     }
 
 }
